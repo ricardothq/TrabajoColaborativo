eceb6faffe8699920f01d839006d91413a26c774
Ramiro <ramirojhoel1998@gmail.com>
2021-10-26 09:54:49 -0400

    parte joel

--- ejercicio1/Program.cs
+++ ejercicio1/Program.cs
@@ -6,7 +6,20 @@ namespace ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int notafinal;
+            float notapractica,notateorica, notaparticipacion;
+            Console.WriteLine("Digite nota practica: ");
+            notapractica=float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite nota participacion: ");
+            notaparticipacion=float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite nota teorica: ");
+            notateorica=float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite nota practica: ");
+            
+
+
+
+
         }
     }
 }
